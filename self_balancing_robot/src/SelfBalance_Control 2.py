6b02af0552144ee4b7273224e867cd31567d2687
cesarwenz <cesarwenzhu5@hotmail.com>
2021-11-25 13:44:40 -0500

    first

---
+++ self_balancing_robot/src/SelfBalance_Control 2.py
@@ -0,0 +1,352 @@
+#!/usr/bin/env python
+
+"""
+Created on Fri Jul 21 18:08:40 2017
+
+@author: sezan92
+"""
+from __future__ import print_function
+import sys,time
+import pidcontrol as pid
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Imu
+from std_msgs.msg import Float32
+from nav_msgs.msg import Odometry
+
+import threading
+# import roslib; roslib.load_manifest('teleop_twist_keyboard')
+import rospy
+from geometry_msgs.msg import Twist
+import sys, select, termios, tty
+
+
+Kp =25
+Ki =0.8
+Kd =0.1
+
+
+pubx = pid.PID_Controller(Kp,Ki,Kd)
+Yaw_Topic = "/yaw"
+cmd_vel = "/cmd_vel"
+Imu_topic = "/imu"
+encoder_topic = "/odom"
+Kp_angle_topic = "/Kp_angle"
+Ki_angle_topic ="/Ki_angle"
+Kd_angle_topic = "/Kd_angle"
+Kp_speed_topic = "/Kp_speed"
+Ki_speed_topic ="/Ki_speed"
+Kd_speed_topic = "/Kd_speed"
+
+
+msg = """
+Reading from the keyboard  and Publishing to Twist!
+---------------------------
+Moving around:
+   u    i    o
+   j    k    l
+   m    ,    .
+For Holonomic mode (strafing), hold down the shift key:
+---------------------------
+   U    I    O
+   J    K    L
+   M    <    >
+t : up (+z)
+b : down (-z)
+anything else : stop
+q/z : increase/decrease max speeds by 10%
+w/x : increase/decrease only linear speed by 10%
+e/c : increase/decrease only angular speed by 10%
+CTRL-C to quit
+"""
+
+moveBindings = {
+        'i':(1,0,0,0),
+        'o':(1,0,0,-1),
+        'j':(0,0,0,1),
+        'l':(0,0,0,-1),
+        'u':(1,0,0,1),
+        ',':(-1,0,0,0),
+        '.':(-1,0,0,1),
+        'm':(-1,0,0,-1),
+        'O':(1,-1,0,0),
+        'I':(1,0,0,0),
+        'J':(0,1,0,0),
+        'L':(0,-1,0,0),
+        'U':(1,1,0,0),
+        '<':(-1,0,0,0),
+        '>':(-1,-1,0,0),
+        'M':(-1,1,0,0),
+        't':(0,0,1,0),
+        'b':(0,0,-1,0),
+    }
+
+speedBindings={
+        'q':(1.1,1.1),
+        'z':(.9,.9),
+        'w':(1.1,1),
+        'x':(.9,1),
+        'e':(1,1.1),
+        'c':(1,.9),
+    }
+
+class PublishThread(threading.Thread):
+    def __init__(self, rate):
+        super(PublishThread, self).__init__()
+        self.publisher = rospy.Publisher('cmd_vel2', Twist, queue_size = 1)
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
+        self.th = 0.0
+        self.speed = 0.0
+        self.turn = 0.0
+        self.condition = threading.Condition()
+        self.done = False
+        self.xvelMin=-.01
+        self.xvelMax =0
+        self.angleMin=-.01
+        self.angleMax =0
+        
+        self.subscriber = rospy.Subscriber(Imu_topic,Imu,self.callback_imu)
+        self.subscriber_Kp_angle = rospy.Subscriber(Kp_angle_topic,Float32,self.callback_Kp_angle)
+        self.subscriber_Ki_angle = rospy.Subscriber(Ki_angle_topic,Float32,self.callback_Ki_angle)
+        self.subscriber_Kd_angle = rospy.Subscriber(Kd_angle_topic,Float32,self.callback_Kd_angle)
+
+        self.subscriber = rospy.Subscriber(encoder_topic,Odometry,self.callback_odom)
+        self.subscriber_Kp_speed = rospy.Subscriber(Kp_speed_topic,Float32,self.callback_Kp_speed)
+        self.subscriber_Ki_speed = rospy.Subscriber(Ki_speed_topic,Float32,self.callback_Ki_speed)
+        self.subscriber_Kd_speed = rospy.Subscriber(Kd_speed_topic,Float32,self.callback_Kd_speed)
+        self.pub1 = rospy.Publisher(Yaw_Topic,Float32,queue_size =1)
+        
+        self.yMin = -0.01
+        self.yMax = -0.001
+        self.yPrev =0
+        self.delY = 0
+        self.Kp_angle = 0
+        self.Ki_angle = 0
+        self.Kd_angle = 0
+        self.Kp_speed = 0
+        self.Ki_speed = 0
+        self.Kd_speed = 0
+
+        self.odom_speed = 0
+        self.read_angle = 0
+
+        self.pubx_angle = pid.PID_Controller(self.Kp_angle,self.Ki_angle,self.Kd_angle)
+        self.pubx_speed = pid.PID_Controller(self.Kp_speed,self.Ki_speed,self.Kd_speed)
+
+        # Set timeout to None if rate is 0 (causes new_message to wait forever
+        # for new data to publish)
+        if rate != 0.0:
+            self.timeout = 1.0 / rate
+        else:
+            self.timeout = None
+
+        self.start()
+
+    def wait_for_subscribers(self):
+        i = 0
+        while not rospy.is_shutdown() and self.publisher.get_num_connections() == 0:
+            if i == 4:
+                print("Waiting for subscriber to connect to {}".format(self.publisher.name))
+            rospy.sleep(0.5)
+            i += 1
+            i = i % 5
+        if rospy.is_shutdown():
+            raise Exception("Got shutdown request before subscribers connected")
+
+    def update(self, x, y, z, th, speed, turn):
+        self.condition.acquire()
+        self.x = x
+        self.y = y
+        self.z = z
+        self.th = th
+        self.speed = speed
+        self.turn = turn
+        # Notify publish thread that we have a new message.
+        self.condition.notify()
+        self.condition.release()
+
+    def stop(self):
+        self.done = True
+        self.update(0, 0, 0, 0, 0, 0)
+        self.join()
+
+    def run(self):
+        twist = Twist()
+
+        
+        
+        while not self.done:
+            setPoint_speed = self.x * self.speed
+
+            angle = -self.pubx_speed.getCorrection(setPoint_speed, self.odom_speed)
+            if angle>self.angleMax:
+                self.angleMax=angle
+            elif self.angleMin<angle:
+                self.angleMin = angle
+            if angle > 0.72:
+                angle = 0.72
+            elif angle < -0.72:
+                angle = -0.72
+            
+            
+            setPoint_angle = angle
+            xvel = -self.pubx_angle.getCorrection(setPoint_angle,self.read_angle)
+            if xvel>self.xvelMax:
+                self.xvelMax=xvel
+            elif xvel<self.xvelMin:
+                self.xvelMin = xvel
+            if xvel >26:
+                xvel =26
+            elif xvel<-26:
+                xvel =-26
+            self.condition.acquire()
+            print(angle)
+            # Wait for a new message or timeout.
+            self.condition.wait(self.timeout)
+
+            # Copy state into twist message.
+            twist.linear.x = xvel
+            twist.linear.y = 0
+            twist.linear.z = 0
+            twist.angular.x = 0
+            twist.angular.y = 0
+            twist.angular.z = self.th * self.turn
+
+            self.condition.release()
+            
+            # Publish.
+            self.publisher.publish(twist)
+
+        # Publish stop message when thread exits.
+        twist.linear.x = 0
+        twist.linear.y = 0
+        twist.linear.z = 0
+        twist.angular.x = 0
+        twist.angular.y = 0
+        twist.angular.z = 0
+        self.publisher.publish(twist)
+    
+    
+    def callback_odom(self, data):
+        self.odom_speed = data.twist.twist.linear.x
+
+    def callback_imu(self,data):
+        self.read_angle = data.orientation.y
+        self.delY = self.read_angle-self.yPrev
+        if self.delY>self.yMax:
+            self.yMax = self.delY
+        elif self.delY<self.yMin:
+            self.yMin = self.delY
+
+        self.yPrev = self.read_angle
+        self.pub1.publish(self.read_angle)
+        #print "Max vel " + str(self.xvelMax) + " & Min vel " + str(self.xvelMin) + " Max delY " + str(self.yMax*180/3.1416) +" & Min delY" + str(self.yMin*180/3.1416)
+    
+    def callback_Kp_angle(self,data):
+        self.Kp_angle = data.data
+        self.pubx_angle = pid.PID_Controller(self.Kp_angle,self.Ki_angle,self.Kd_angle)
+    
+    def callback_Ki_angle(self,data):
+        self.Ki_angle = data.data
+        self.pubx_angle = pid.PID_Controller(self.Kp_angle,self.Ki_angle,self.Kd_angle)
+    
+    def callback_Kd_angle(self,data):
+        self.Kd_angle = data.data
+        self.pubx_angle = pid.PID_Controller(self.Kp_angle,self.Ki_angle,self.Kd_angle)
+
+    def callback_Kp_speed(self,data):
+        self.Kp_speed = data.data
+        self.pubx_speed = pid.PID_Controller(self.Kp_speed,self.Ki_speed,self.Kd_speed)
+    
+    def callback_Ki_speed(self,data):
+        self.Ki_speed = data.data
+        self.pubx_speed = pid.PID_Controller(self.Kp_speed,self.Ki_speed,self.Kd_speed)
+    
+    def callback_Kd_speed(self,data):
+        self.Kd_speed = data.data
+        self.pubx_speed = pid.PID_Controller(self.Kp_speed,self.Ki_speed,self.Kd_speed)
+
+        
+
+def getKey(key_timeout):
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], key_timeout)
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+
+def vels(speed, turn):
+    return "currently:\tspeed %s\tturn %s " % (speed,turn)
+
+
+def main():
+    '''Initializes and cleanup ros node'''
+    rospy.init_node('SelfBalance', anonymous=True)
+
+    speed = rospy.get_param("~speed", 0.5)
+    turn = rospy.get_param("~turn", 1.0)
+    repeat = rospy.get_param("~repeat_rate", 20)
+    key_timeout = rospy.get_param("~key_timeout", 0.0)
+    if key_timeout == 0.0:
+        key_timeout = None
+
+    pub_thread = PublishThread(repeat)
+
+    x = 0
+    y = 0
+    z = 0
+    th = 0
+    status = 0
+
+    
+    try:
+        pub_thread.wait_for_subscribers()
+        pub_thread.update(x, y, z, th, speed, turn)
+        print(msg)
+        print(vels(speed,turn))
+        while(1):
+            key = getKey(key_timeout)
+            if key in moveBindings.keys():
+                x = moveBindings[key][0]
+                y = moveBindings[key][1]
+                z = moveBindings[key][2]
+                th = moveBindings[key][3]
+            elif key in speedBindings.keys():
+                speed = speed * speedBindings[key][0]
+                turn = turn * speedBindings[key][1]
+
+                print(vels(speed,turn))
+                if (status == 14):
+                    print(msg)
+                status = (status + 1) % 15
+            else:
+                # Skip updating cmd_vel if key timeout and robot already
+                # stopped.
+                if key == '' and x == 0 and y == 0 and z == 0 and th == 0:
+                    continue
+                x = 0
+                y = 0
+                z = 0
+                th = 0
+                if (key == '\x03'):
+                    break
+ 
+            pub_thread.update(x, y, z, th, speed, turn)
+
+    except Exception as e:
+        print(e)
+    finally:
+        pub_thread.stop()
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
+if __name__ == '__main__':
+    settings = termios.tcgetattr(sys.stdin)
+    main()
+
+# %%
